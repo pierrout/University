246eac5eeaff9b84d885c5af69e66de3d9c95333
Ana Flávia <64327735+pierrout@users.noreply.github.com>
2021-04-27 10:58:20 -0300

    Create Intenger.java

---
+++ Numerosmultiplos/Intenger.java
@@ -0,0 +1,21 @@
+package numerosmultiplos;
+
+import javax.swing.JOptionPane;
+
+public class Intenger {
+
+    
+     public void displayMessage1() {
+        
+        String mensagem = "são multiplos!";
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
+     
+     public void displayMessage2() {
+        
+        String mensagem = "não são multiplos!";
+        JOptionPane.showMessageDialog(null, mensagem);
+    }
+
+    
+}
