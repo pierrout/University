0c8db0b7a778b0c5dd68bef14930a2e3742f4147
Ana Flávia <pierrout77@gmail.com>
2021-05-04 12:03:43 -0300

    Create Funcionario2.java

---
+++ funcionario/Funcionario2.java
@@ -0,0 +1,44 @@
+package funcionario;
+
+public class Funcionario2 {
+   private String nome;
+   private int telefone;
+   private String sexo; 
+   private double salarioBase;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+   
+   
+    
+    
+}
